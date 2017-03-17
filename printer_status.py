be2023b849b361d8471323f1390ebf6befe37ad7
Torgeir Lebesbye <torgeir@lebesbye.com>
2017-03-17 13:18:21 +0100

    printer_status.py can now fetch some basic information from a Ricoh printer

--- printer_status.py
+++ printer_status.py
@@ -1,18 +1,19 @@
-from netsnmp import Varbind, VarList, snmpget, snmpgetnext
+from netsnmp import Varbind, VarList, snmpget
+from os import environ
 from subprocess import CalledProcessError, check_output, STDOUT
 from sys import argv, exit
 
 usage = 'Usage: %s <printer>...' % argv[0]
 
-def get_by_oid(printer_name, oid):
-    '''Returns the value of a given Object Identifier (OID) for a printer'''
-    return snmpget(Varbind(oid), DestHost = printer_name, Community = 'public', Version = 1)
+# Provide the paths to needed MIBs with a comma (!) separeted list
+mibs_to_load = '../mibs/Printer-MIB.my:../mibs/SNMPv2-MIB.txt'
 
-def get_next_by_oid(printer_name, oid):
-    '''Returns the value of a given Object Identifier (OID) for a printer'''
-    return snmpgetnext(Varbind(oid), DestHost = printer_name, Community = 'public', Version = 1)
+def get_by_mib(printer_name, mib):
+    '''Returns the value of a given Management Information Base (MIB) for a printer'''
+    return snmpget(Varbind(mib), DestHost = printer_name, Community = 'public', Version = 1)
 
 def ping(host):
+    '''Silently pings the host once. Returns true if host answers; false if it doesn't.'''
     try:
         check_output(['ping', '-c', '1', host], stderr=STDOUT, universal_newlines=True)
     except CalledProcessError:
@@ -20,6 +21,11 @@ def ping(host):
     return True
 
 def get_printer_info(printer_name):
+    '''Queries a printer and returns its info ready to print.'''
+    # Loading MIBs with environ is a bit hacky, but worked.
+    # TODO: configure netsnmp properly
+    environ['MIBS'] = mibs_to_load
+
     # TODO: use VarList(Varbind<>...) to bulk all SNMP queries for performance boost
     #
     # ... but it's not yet supported: "Note that only one varbind should be contained 
@@ -27,39 +33,34 @@ def get_printer_info(printer_name):
     # future, but right now walking multiple trees at once is not yet supported and 
     # will produce insufficient results."
 
-    model = get_next_by_oid(printer_name, '.1.3.6.1.2.1.25.3.2.1.3')
-    lines = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.5.1.1.11') # TODO: not in use??!
-    chars = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.5.1.1.12')
-    display = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.16.5.1.2')
+    model = map(str.strip, get_by_mib(printer_name, 'sysDescr.0')[0].split('/'))
+    try:
+        location = get_by_mib(printer_name, 'sysLocation.0')[0].split(',')[2]
+    except IndexError:
+        location = None
+    display = get_by_mib(printer_name, 'prtConsoleDisplayBufferText.1.1')
+    supply_level_black = get_by_mib(printer_name, 'prtMarkerSuppliesLevel.1.1')[0]
+    supply_level_cyan = get_by_mib(printer_name, 'prtMarkerSuppliesLevel.1.3')[0]
+    supply_level_magenta = get_by_mib(printer_name, 'prtMarkerSuppliesLevel.1.4')[0]
+    supply_level_yellow = get_by_mib(printer_name, 'prtMarkerSuppliesLevel.1.5')[0]
+    supply_level_waste = get_by_mib(printer_name, 'prtMarkerSuppliesLevel.1.2')[0]
+    page_count = get_by_mib(printer_name, 'prtMarkerLifeCount.1.1')[0]
 
-    supply_desc = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.11.1.1.6')
-    supply_max = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.11.1.1.8')
-    supply_level = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.11.1.1.9')
-
-    tray_unit = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.8')
-    tray_max = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.9')
-    tray_level = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.10')
-    tray_status = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.11')
-    tray_type = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.12')
-    tray_tray = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.13')
-
-    size_x = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.7')
-    size_y = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.6')
-    size_type = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.8.2.1.3')
-
-    page_count = get_next_by_oid(printer_name, '.1.3.6.1.2.1.43.10.2.1.4')
-
-    info = 'Querying \033[1m' + printer_name.split('.')[0] + '\033[0m' + ':'
-    info += '\nModel:\n'
+    info = 'Querying \033[1m' + printer_name.split('.')[0] + '\033[0m'
+    if location:
+        info += ' (rom ' + location +')'
+    info += ':\nModel:\n'
     for line in model:
         info += '   %s\n' % line
     info += 'Display:\n'
-    for i, element in enumerate(display):
-        info += '%4i: [ %-*s ]\n' % (i+1, int(chars[0]), element)
-    info += 'Supply status for %s:\n' % printer_name
-        #TODO
-    info += 'Tray status for %s:\n' % printer_name
-        #TODO
+    for line in display:
+        info += '   %s\n' % line
+    info += 'Supply status:\n'
+    info += '%6i %% Black\n' % int(supply_level_black)
+    info += '%6i %% Cyan\n' % int(supply_level_cyan)
+    info += '%6i %% Magenta\n' % int(supply_level_magenta)
+    info += '%6i %% Yellow\n' % int(supply_level_yellow)
+    info += '%6i %% Waste\n' % int(supply_level_waste)
     info += 'Page count: %s\n' % page_count
     return info
 
