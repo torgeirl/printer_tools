bc7b8f8622588148aa915dbfaea68aa14b27eec8
Torgeir Lebesbye <torgeir@lebesbye.com>
2017-06-30 14:58:16 +0200

    Added a flag to control the number of pings. More pings are costly (for 15 printers, about 15sec extra per ping after the first, that only takes ~1 sec), but should reduce the number of false positives

--- printer_monitor.py
+++ printer_monitor.py
@@ -21,7 +21,7 @@ mibs_dir = path.abspath(path.join(path.dirname( __file__ ), '..', 'mibs'))
 mibs_to_load = mibs_dir + '/Printer-MIB.my:' + mibs_dir + '/DISMAN-EVENT-MIB.txt' #colon-separated (!) list
 environ['MIBS'] = mibs_to_load
 
-ignore_list = 'low:|lite:|no paper|tomt for papir|low power mode|energy saver mode|energisparemodus|modus for lavt str|warming up|varmer opp|nearly full|nesten full|not detected: tray|finner ikke: magasin|not detected: input|mismatch: paper size and type|passer ikke: papirformat og type|current job suspended|adjusting|toner fra uavhengig leverand'
+ignore_list = 'low:|lite:|no paper|tomt for papir|low power mode|energy saver mode|energisparemodus|modus for lavt str|warming up|varmer opp|nearly full|nesten full|not detected: tray|finner ikke: magasin|not detected: input|mismatch: paper size and type|passer ikke: papirformat og type|current job suspended|adjusting|offline|frakoblet'
 
 def ping(host, times=1):
     '''Silently pings the host once. Returns true if host answers; false if it doesn't.'''
@@ -39,30 +39,34 @@ def get_mib(device_address, mib):
     '''Returns the value of the given Management Information Base (MIB) for a network device'''
     return snmpget(Varbind(mib), DestHost = device_address, Community = 'public', Version = 1)
 
-def get_printer_errors(printer_address, ignore_list, all=False):
+def get_printer_errors(printer_address, ignore_list, all=False, pings=1):
     '''Returns all errors from a printer'''
-    if ping(printer_address): #TODO: impliment some actual error handling
+    if ping(printer_address, pings): #TODO: impliment some actual error handling
         err_descriptions = list(walk_mib(printer_address, 'prtAlertDescription.1'))
         err_ticks = list(walk_mib(printer_address, 'prtAlertTime.1'))
         if err_descriptions:
             parsed_errors = ''
             system_uptime_ticks = int(get_mib(printer_address, 'sysUpTimeInstance')[0])
+            location = get_mib(printer_address, 'sysLocation.0')[0].split(',')[2]
             for index, description in enumerate(err_descriptions):
                 err_desc = description.split('{')[0] 
                 if all or not search(ignore_list, err_desc.lower()):
                     err_time = str(timedelta(seconds=(system_uptime_ticks - int(err_ticks[index]))/100))
-                    parsed_errors += '[%s] %s in %s\n' % (printer_address.split('.')[0].upper(), err_desc, err_time)
+                    parsed_errors += '%s (%s): %s in %s\n' % (printer_address.split('.')[0].upper(), location, err_desc, err_time)
             return parsed_errors
         else:
             return ''
     else:
-        return '[%s] host \'%s\' unknown or offline\n' % (printer_address.split('.')[0].upper(), printer_address)
+        return '%s: host \'%s\' unknown or offline\n' % (printer_address.split('.')[0].upper(), printer_address)
 
 if __name__ == '__main__':
     parser = ArgumentParser(usage=usage)
     parser.add_argument('-a', '--all',
         default=False,
         help='Display all errors')
+    parser.add_argument('-p', '--pings',
+        default=1,
+        help='More pings takes more time, but gives less false positives')
     parser.add_argument('args', nargs=REMAINDER)
     args = parser.parse_args()
 
@@ -70,7 +74,7 @@ if __name__ == '__main__':
         start_time = time.time()
         errors = ''
         for arg in args.args:
-            errors += get_printer_errors(arg, ignore_list, args.all)
+            errors += get_printer_errors(arg, ignore_list, args.all, args.pings)
         end_time = time.time()
         if len(errors) > 0:
             print errors.replace('\xe6', 'ae').replace('\xf8', 'oe').replace('\xe5', 'aa')
